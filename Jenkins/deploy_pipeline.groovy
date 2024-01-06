e1fd641be64e8871545407771e04a4c145b653c4
smithbot <dansmithbot@gmail.com>
2024-01-05 21:07:50 -0500

    fixed timestamp

--- Jenkins/deploy_pipeline.groovy
+++ Jenkins/deploy_pipeline.groovy
@@ -4,6 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
+                // Pull the code from the main branch of your Git repository using SSH credentials
                 script {
                     git branch: 'main', credentialsId: 'bc55c06a-9c3c-4734-8c20-451153d96c5c', url: 'https://github.com/dansmithbot/your_hello_world_app.git'
                 }
@@ -12,18 +13,21 @@ pipeline {
         
         stage('Prepare') {
             steps {
+                // Create target directory
                 sh 'mkdir -p target'
             }
         }
         
         stage('Build') {
             steps {
+                // Compile Java source files - Adjust based on your Java project structure
                 dir('src') {
                     sh 'javac -d ../target HelloWorld.java'
                 }
 
+                // Generate timestamp for versioning
                 dir('target') {
-                    def timestamp = sh(script: 'date +%Y%m%d%H%M%S', returnStdout: true).trim()
+                    timestamp = sh(script: 'date +%Y%m%d%H%M%S', returnStdout: true).trim()
                     sh "jar cvf hello-world-${timestamp}.jar -C ../target HelloWorld.class"
                 }
             }
@@ -31,7 +35,10 @@ pipeline {
         
         stage('Deploy to Raspberry Pi') {
             steps {
+                // List contents of the target directory for debugging
                 sh 'ls -l target/'
+                
+                // Move the built artifact to the 'artifacts' subfolder
                 sh 'mv target/hello-world-*.jar target/artifacts/'
             }
         }
