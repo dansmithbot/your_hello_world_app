a821756cefdd28f26284f0020dc6bc6ee11b3ec3
smithbot <dansmithbot@gmail.com>
2024-01-05 21:04:07 -0500

    change way timestamp is created and mod hello msg

--- Jenkins/deploy_pipeline.groovy
+++ Jenkins/deploy_pipeline.groovy
@@ -4,7 +4,6 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                // Pull the code from the main branch of your Git repository using SSH credentials
                 script {
                     git branch: 'main', credentialsId: 'bc55c06a-9c3c-4734-8c20-451153d96c5c', url: 'https://github.com/dansmithbot/your_hello_world_app.git'
                 }
@@ -13,23 +12,18 @@ pipeline {
         
         stage('Prepare') {
             steps {
-                // Create target directory
                 sh 'mkdir -p target'
             }
         }
         
         stage('Build') {
             steps {
-                // Compile Java source files - Adjust based on your Java project structure
                 dir('src') {
                     sh 'javac -d ../target HelloWorld.java'
                 }
-                
-                // Create a timestamp for versioning
-                def timestamp = sh(script: 'date +%Y%m%d%H%M%S', returnStdout: true).trim()
-                
-                // Create the JAR file with a timestamp in the name
+
                 dir('target') {
+                    def timestamp = sh(script: 'date +%Y%m%d%H%M%S', returnStdout: true).trim()
                     sh "jar cvf hello-world-${timestamp}.jar -C ../target HelloWorld.class"
                 }
             }
@@ -37,10 +31,7 @@ pipeline {
         
         stage('Deploy to Raspberry Pi') {
             steps {
-                // List contents of the target directory for debugging
                 sh 'ls -l target/'
-                
-                // Move the built artifact to the 'artifacts' subfolder
                 sh 'mv target/hello-world-*.jar target/artifacts/'
             }
         }
