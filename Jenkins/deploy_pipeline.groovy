ea1c5a4b50933230b3c8c1f4ae347412a181b6bb
smithbot <dansmithbot@gmail.com>
2024-04-11 14:41:08 -0400

    remove all the files it's made in the last month

--- Jenkins/deploy_pipeline.groovy
+++ Jenkins/deploy_pipeline.groovy
@@ -14,33 +14,34 @@ pipeline {
         stage('Prepare') {
             steps {
                 // Create target directory
-                sh 'mkdir -p target'
+                // sh 'mkdir -p target'
+                sh 'rm -rf target'
             }
         }
         
-        stage('Build') {
-            steps {
-                // Compile Java source files - Adjust based on your Java project structure
-                dir('src') {
-                    sh 'javac -d ../target HelloWorld.java'
-                }
+        // stage('Build') {
+        //     steps {
+        //         // Compile Java source files - Adjust based on your Java project structure
+        //         dir('src') {
+        //             sh 'javac -d ../target HelloWorld.java'
+        //         }
 
-                // Generate timestamp for versioning
-                dir('target') {
-                    timestamp = sh(script: 'date +%Y%m%d%H%M%S', returnStdout: true).trim()
-                    sh "jar cvf hello-world-${timestamp}.jar -C ../target HelloWorld.class"
-                }
-            }
-        }
+        //         // Generate timestamp for versioning
+        //         dir('target') {
+        //             timestamp = sh(script: 'date +%Y%m%d%H%M%S', returnStdout: true).trim()
+        //             sh "jar cvf hello-world-${timestamp}.jar -C ../target HelloWorld.class"
+        //         }
+        //     }
+        // }
         
-        stage('Deploy to Raspberry Pi') {
-            steps {
-                // List contents of the target directory for debugging
-                sh 'ls -l target/'
+        // stage('Deploy to Raspberry Pi') {
+        //     steps {
+        //         // List contents of the target directory for debugging
+        //         sh 'ls -l target/'
                 
-                // Move the built artifact to the 'artifacts' subfolder
-                sh 'mv target/hello-world-*.jar target/artifacts/'
-            }
-        }
+        //         // Move the built artifact to the 'artifacts' subfolder
+        //         sh 'mv target/hello-world-*.jar target/artifacts/'
+        //     }
+        // }
     }
 }
